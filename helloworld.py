651edd7afe23ce74175b73ffb2995815b88bf63f
Mario Zelaya <mariozelaya23@gmail.com>
2021-11-30 14:34:47 -0800

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,26 +1,9 @@
-<< << << < HEAD
-
-
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-== == == =
-
-
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
-
-
->>>>>> > 3cd8e501240c3c2d1d120f1125c52c97634a1564
 
 
 main()
